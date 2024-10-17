8a8d490bf725af610cccf26878db515c4b8f3300
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-10-17 22:14:24 +0200

    feat: Add service_index to RegularService

--- src/transport/serice_index.rs
+++ src/transport/serice_index.rs
@@ -1,7 +1,7 @@
 #[derive(Debug)]
 pub enum ServiceIndex {
     ControlService,
-    RegularService,
+    RegularService(u8),
     PipePacket,
     AckPacket,
     Reserved,
@@ -12,7 +12,7 @@ impl From<u8> for ServiceIndex {
         if value == 0x00 {
             ServiceIndex::ControlService
         } else if 0x01 <= value && value <= 0x3A {
-            ServiceIndex::RegularService
+            ServiceIndex::RegularService(value)
         } else if value == 0x3E {
             ServiceIndex::PipePacket
         } else if value == 0x3F {
