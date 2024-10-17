17de766f02ebca1854a13db0c68a6206f8c27f5a
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-10-17 22:18:35 +0200

    feat: Creat struct to reprensent Brain

---
+++ src/brain.rs
@@ -0,0 +1,35 @@
+use alloc::vec::Vec;
+// use circular_buffer::CircularBuffer;
+
+use crate::{brain_device::BrainDevice, brain_error::BrainError, transport::frame::Frame};
+
+pub const BRAIN_EVENT_QUEUE_SIZE: usize = 16;
+
+#[derive(Debug, Default)]
+pub struct Brain {
+    pub known_devices: Vec<BrainDevice>,
+    // pub event_queue: CircularBuffer<BRAIN_EVENT_QUEUE_SIZE, EventReport>,
+}
+
+impl Brain {
+    pub fn handle_frame(&mut self, frame: Frame) -> Result<(), BrainError> {
+        match self.get_device_index_by_id(frame.device_id) {
+            Some(idx) => {
+                self.known_devices[idx].handle_frame(frame)?;
+            }
+            None => {
+                let mut device = BrainDevice::new(frame.device_id);
+                device.handle_frame(frame)?;
+                self.known_devices.push(device);
+            }
+        };
+        Ok(())
+    }
+
+    fn get_device_index_by_id(&self, id: u64) -> Option<usize> {
+        self.known_devices.iter().position(|x| x.id() == id)
+    }
+
+    // pub fn clean_devices(&mut self){
+    // }
+}
